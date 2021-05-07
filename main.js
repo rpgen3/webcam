6ccd43e675598f12a30c6cb496cd19061b56d1dc
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-05-07 11:52:40 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-let isLoaded = false;
-const Photo = 0,
-      Video = 1;
-let mode = Photo;
 const { $, rpgen3 } = window;
 const h = $("<div>").appendTo($("body")).css({
     "text-align": "center",
     padding: "1em"
 });
-$("<h1>").appendTo(h).text("webカメラのサンプル");
+$("<h1>").appendTo(h).text("webカメラの実験");
 const hMsg = $("<div>").appendTo(h);
 function msg(str, isError){
     $("<span>").appendTo(hMsg.empty()).text(str).css({
@@ -18,17 +14,13 @@ function msg(str, isError){
 const isFront = rpgen3.addInputBool(h,{
     title: "前面カメラを使用"
 });
-const {width, height} = {
-    width: 640,
-    height: 480,
-};
 let stream;
 const disabled = b => $("button").attr("disabled",b);
 $("<button>").appendTo(h).text("カメラの許可").on("click", async()=>{
     disabled(true);
     try {
+        if(stream) stream.getTracks().forEach(track => track.stop());
         stream = await navigator.mediaDevices.getUserMedia({
-            width, height,
             video: isFront() ? "user" : {facingMode: "environment"},
             audio: true
         });
@@ -45,29 +37,23 @@ $("<button>").appendTo(h).text("カメラの許可").on("click", async()=>{
     disabled(false);
 });
 $("<button>").appendTo(h).text("撮影").on("click",()=>{
-    img.attr("src", cv.toDataURL('image/png'));
-    mode = Photo;
+    img.src = cv.toDataURL('image/png');
 });
 const isNowREC = rpgen3.addInputBool(h,{
     title: "録画",
     change: v => {
-        if(!isLoaded || !stream) return false;
+        if(!stream) return false;
         REC[v ? 'start' : 'stop']();
     }
 });
 $("<button>").appendTo(h).text("再生").on("click", () => REC.play());
-$("<button>").appendTo(h).text("保存").on("click", ()=>{
-    const b = mode === Photo;
-    $("<a>").attr({
-        href: b ? img.attr("src") : videoREC.src,
-        download: b ? 'webcam.png' : 'webcam.webm'
-    }).get(0).click();
-});
+const DL = (href, download) => $("<a>").attr({ href, download }).get(0).click();
 $("<h3>").appendTo(h).text("<video>");
 const makeVideo = () => $("<video>").appendTo(h).attr({ autoplay: true }).get(0),
       video = makeVideo(),
       videoREC = makeVideo();
-const cv = $("<canvas>").attr({width, height}).get(0),
+$("<button>").appendTo(h).text("動画を保存").on("click",()=>DL(videoREC.src, 'webcam.webm'));
+const cv = $("<canvas>").get(0),
       ctx = cv.getContext('2d');
 (function update(){
     ctx.drawImage(video, 0, 0);
@@ -89,7 +75,6 @@ const REC = (()=>{
             blobURL = window.URL.createObjectURL(new Blob(blobs, {
                 type: "video/webm"
             }));
-            mode = Video;
         },
         play: ()=>{
             if(isNowREC()) return msg("録画中です", true);
@@ -100,5 +85,5 @@ const REC = (()=>{
     }
 })();
 $("<h3>").appendTo(h).text("<img>");
-const img = $("<img>").appendTo(h);
-isLoaded = true;
+const img = $("<img>").appendTo(h).get(0);
+$("<button>").appendTo(h).text("画像を保存").on("click",()=>DL(img.src, 'webcam.png'));
